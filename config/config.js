5108d6b147c8640ae147403e8e313d9ccff5b090
A Hoerman <awhoerman@me.com>
2020-04-18 13:50:14 -0400

    changes to get aws deployed

--- config/config.js
+++ config/config.js
@@ -5,24 +5,24 @@ module.exports=
   "development": {
     "username": process.env.username,
     "password": process.env.password,
-    "database": "database-1",
+    "database": "database1",
     "host": process.env.host,
     "port": 3306,
     "dialect": "mysql"
   },
   "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
+    "username": process.env.username,
+    "password": process.env.password,
+    "database": "database1",
+    "host": process.env.host,
     "port": 3306,
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "username": process.env.username,
+    "password": process.env.password,
+    "database": "database1",
+    "host": process.env.host,
     "port": 3306,
     "dialect": "mysql"
   }
